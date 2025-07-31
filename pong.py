d384b800cb7868667123bef63fdc47daf50a6647
giannag27 <giacosey28@gmail.com>
2025-07-31 16:20:02 -0400

    imported pygame and random. set up screen size and colors.

---
+++ pong.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_model((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Pong')
+
+if __name__ == '__main__':
+    main()
+
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 720
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_PURPLE = (96, 73, 100)
