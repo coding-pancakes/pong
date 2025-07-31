dfb2c7ddcb6df4c77bb8e7a5d82229f3dc3cd4e5
giannag27 <giacosey28@gmail.com>
2025-07-31 16:28:30 -0400

    made loop

--- pong.py
+++ pong.py
@@ -6,6 +6,14 @@ def main():
     screen = pygame.display.set_model((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Pong')
 
+    # Loop
+    while True:
+        screen.fill(COLOR_BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
 if __name__ == '__main__':
     main()
 
