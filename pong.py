c14511f799fefa7db5478797c20637d552f5c670
giannag27 <giacosey28@gmail.com>
2025-08-01 10:31:25 -0400

    paddle direction and size created. ball size, speed, and direction.

--- pong.py
+++ pong.py
@@ -2,9 +2,28 @@ import pygame
 import random
 
 def main():
+    #Setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Pong')
+
+    #PADDLES
+    paddle_1_rect = pygame.Rect(30, 0, 7, 100)
+    paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
+
+    paddle_1_move = 0
+    paddle_2_move = 0
+
+    #BALL
+    ball_rect = ptgame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+
+    ball_accel_x = random.randint(2, 4) * 0.1
+    ball_accel_x = random.randint(2, 4) * 0.1
+
+    if random.randint(1, 2) == 1:
+        ball_accel_x *= -1
+    elif random.randint(1, 2) ==1:
+        ball_accel_y *= -1
 
     # Loop
     while True:
